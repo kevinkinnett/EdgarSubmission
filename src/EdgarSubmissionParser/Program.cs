7a4d81aacb960086a23c6479d527e008e081ab57
Kevin Kinnett <kevin.kinnett@assurant.com>
2022-02-16 07:14:05 -0500

    Adding in Dependency Injection

--- src/EdgarSubmissionParser/Program.cs
+++ src/EdgarSubmissionParser/Program.cs
@@ -4,52 +4,75 @@ using System.Reflection;
 using System.Xml.Serialization;
 using AutoMapper;
 using ConsoleApp5.Model;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+using Microsoft.Extensions.Options;
 
-Console.WriteLine("Hello, World!");
 
-edgarSubmission submission;
-
-var serializer = new XmlSerializer(typeof(edgarSubmission));
-using (var fileStream = new FileStream("Data/primary_doc.xml", FileMode.Open))
+public class Program
 {
-    submission = (edgarSubmission)serializer.Deserialize(fileStream);
+    static void Main()
+    {
+        var serviceProvider = new ServiceCollection()
+            .AddLogging(builder => builder.AddConsole())
+            .BuildServiceProvider();
+
+        //configure console logging
+        var logger = serviceProvider
+            .GetService<ILoggerFactory>()!.CreateLogger<Program>();
+
+        logger.LogInformation("Hello, World!");
+
+        edgarSubmission submission;
+
+        var serializer = new XmlSerializer(typeof(edgarSubmission));
+        using (var fileStream = new FileStream("Data/primary_doc.xml", FileMode.Open))
+        {
+            submission = (edgarSubmission)serializer.Deserialize(fileStream);
+        }
+
+        //submission.formData.invstOrSecs
+        var config = new MapperConfiguration(cfg =>
+            cfg.CreateMap<edgarSubmissionFormDataInvstOrSec, Output>()
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.name))
+                .ForMember(dest => dest.Lei, opt => opt.MapFrom(src => src.lei))
+                .ForMember(dest => dest.Title, opt => opt.MapFrom(src => src.title))
+                .ForMember(dest => dest.Cusip, opt => opt.MapFrom(src => src.cusip))
+                .ForMember(dest => dest.Balance, opt => opt.MapFrom(src => src.balance))
+                .ForMember(dest => dest.Units, opt => opt.MapFrom(src => src.units))
+                .ForMember(dest => dest.CurCd, opt => opt.MapFrom(src => src.curCd))
+                .ForMember(dest => dest.ValUsd, opt => opt.MapFrom(src => src.valUSD))
+                .ForMember(dest => dest.PctVal, opt => opt.MapFrom(src => src.pctVal))
+                .ForMember(dest => dest.PayoffProfile, opt => opt.MapFrom(src => src.payoffProfile))
+                .ForMember(dest => dest.AssetCat, opt => opt.MapFrom(src => src.assetCat))
+                .ForMember(dest => dest.InvCountry, opt => opt.MapFrom(src => src.invCountry))
+                .ForMember(dest => dest.IsRestrictedSec, opt => opt.MapFrom(src => src.isRestrictedSec))
+                .ForMember(dest => dest.FairValLevel, opt => opt.MapFrom(src => src.fairValLevel))
+            );
+
+        var mapper = config.CreateMapper();
+        var dto = mapper.Map<Output[]>(submission.formData.invstOrSecs);
+
+        WriteCsv(dto, "Data/output.csv");
+        logger.LogInformation("end");
+
+
+    }
+
+    static void WriteCsv<T>(IEnumerable<T> items, string path)
+    {
+        var itemType = typeof(T);
+        var props = itemType.GetProperties(BindingFlags.Public | BindingFlags.Instance)
+            .OrderBy(p => p.Name);
+
+        using var writer = new StreamWriter(path);
+
+        writer.WriteLine(string.Join(", ", props.Select(p => p.Name)));
+
+        foreach (var item in items)
+            writer.WriteLine(string.Join(", ", props.Select(p => p.GetValue(item, null))));
+    }
 }
 
-//submission.formData.invstOrSecs
-var config = new MapperConfiguration(cfg => 
-    cfg.CreateMap<edgarSubmissionFormDataInvstOrSec, Output>()
-        .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.name))
-        .ForMember(dest => dest.Lei, opt => opt.MapFrom(src => src.lei))
-        .ForMember(dest => dest.Title, opt => opt.MapFrom(src => src.title))
-        .ForMember(dest => dest.Cusip, opt => opt.MapFrom(src => src.cusip))
-        .ForMember(dest => dest.Balance, opt => opt.MapFrom(src => src.balance))
-        .ForMember(dest => dest.Units, opt => opt.MapFrom(src => src.units))
-        .ForMember(dest => dest.CurCd, opt => opt.MapFrom(src => src.curCd))
-        .ForMember(dest => dest.ValUsd, opt => opt.MapFrom(src => src.valUSD))
-        .ForMember(dest => dest.PctVal, opt => opt.MapFrom(src => src.pctVal))
-        .ForMember(dest => dest.PayoffProfile, opt => opt.MapFrom(src => src.payoffProfile))
-        .ForMember(dest => dest.AssetCat, opt => opt.MapFrom(src => src.assetCat))
-        .ForMember(dest => dest.InvCountry, opt => opt.MapFrom(src => src.invCountry))
-        .ForMember(dest => dest.IsRestrictedSec, opt => opt.MapFrom(src => src.isRestrictedSec))
-        .ForMember(dest => dest.FairValLevel, opt => opt.MapFrom(src => src.fairValLevel))
-    );
 
-var mapper = config.CreateMapper();
-var dto = mapper.Map<Output[]>(submission.formData.invstOrSecs);
-
-WriteCsv(dto, "Data/output.csv");
-Console.WriteLine("end");
-
-void WriteCsv<T>(IEnumerable<T> items, string path)
-{
-    var itemType = typeof(T);
-    var props = itemType.GetProperties(BindingFlags.Public | BindingFlags.Instance)
-        .OrderBy(p => p.Name);
-
-    using var writer = new StreamWriter(path);
-
-    writer.WriteLine(string.Join(", ", props.Select(p => p.Name)));
-
-    foreach (var item in items)
-        writer.WriteLine(string.Join(", ", props.Select(p => p.GetValue(item, null))));
-}
