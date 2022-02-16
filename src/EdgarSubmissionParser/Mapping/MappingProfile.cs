ed9dfb46a3397285015e7837866c66458dc290c7
Kevin Kinnett <kevin.kinnett@assurant.com>
2022-02-16 07:24:14 -0500

    Adding Maping to DI

---
+++ src/EdgarSubmissionParser/Mapping/MappingProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using ConsoleApp5.Model;
+
+namespace EdgarSubmissionParser.Mapping;
+
+public class MappingProfile: Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<edgarSubmissionFormDataInvstOrSec, Output>()
+            .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.name))
+            .ForMember(dest => dest.Lei, opt => opt.MapFrom(src => src.lei))
+            .ForMember(dest => dest.Title, opt => opt.MapFrom(src => src.title))
+            .ForMember(dest => dest.Cusip, opt => opt.MapFrom(src => src.cusip))
+            .ForMember(dest => dest.Balance, opt => opt.MapFrom(src => src.balance))
+            .ForMember(dest => dest.Units, opt => opt.MapFrom(src => src.units))
+            .ForMember(dest => dest.CurCd, opt => opt.MapFrom(src => src.curCd))
+            .ForMember(dest => dest.ValUsd, opt => opt.MapFrom(src => src.valUSD))
+            .ForMember(dest => dest.PctVal, opt => opt.MapFrom(src => src.pctVal))
+            .ForMember(dest => dest.PayoffProfile, opt => opt.MapFrom(src => src.payoffProfile))
+            .ForMember(dest => dest.AssetCat, opt => opt.MapFrom(src => src.assetCat))
+            .ForMember(dest => dest.InvCountry, opt => opt.MapFrom(src => src.invCountry))
+            .ForMember(dest => dest.IsRestrictedSec, opt => opt.MapFrom(src => src.isRestrictedSec))
+            .ForMember(dest => dest.FairValLevel, opt => opt.MapFrom(src => src.fairValLevel));
+    }
+}
--- src/EdgarSubmissionParser/Program.cs
+++ src/EdgarSubmissionParser/Program.cs
@@ -4,6 +4,7 @@ using System.Reflection;
 using System.Xml.Serialization;
 using AutoMapper;
 using ConsoleApp5.Model;
+using EdgarSubmissionParser.Mapping;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Console;
@@ -16,6 +17,14 @@ public class Program
     {
         var serviceProvider = new ServiceCollection()
             .AddLogging(builder => builder.AddConsole())
+            .AddSingleton(factory =>
+            {
+                var config = new MapperConfiguration(
+                    cfg => { cfg.AddProfile<MappingProfile>(); });
+                config.AssertConfigurationIsValid();
+                return config.CreateMapper();
+
+            })
             .BuildServiceProvider();
 
         //configure console logging
@@ -24,31 +33,12 @@ public class Program
 
         logger.LogInformation("Hello, World!");
 
-        edgarSubmission submission;
+        var submission = DeserializeXml<edgarSubmission>("Data/primary_doc.xml");
 
-        submission = DeserializeXml<edgarSubmission>("Data/primary_doc.xml");
+        var mapper = serviceProvider
+            .GetService<IMapper>();
 
-        //submission.formData.invstOrSecs
-        var config = new MapperConfiguration(cfg =>
-            cfg.CreateMap<edgarSubmissionFormDataInvstOrSec, Output>()
-                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.name))
-                .ForMember(dest => dest.Lei, opt => opt.MapFrom(src => src.lei))
-                .ForMember(dest => dest.Title, opt => opt.MapFrom(src => src.title))
-                .ForMember(dest => dest.Cusip, opt => opt.MapFrom(src => src.cusip))
-                .ForMember(dest => dest.Balance, opt => opt.MapFrom(src => src.balance))
-                .ForMember(dest => dest.Units, opt => opt.MapFrom(src => src.units))
-                .ForMember(dest => dest.CurCd, opt => opt.MapFrom(src => src.curCd))
-                .ForMember(dest => dest.ValUsd, opt => opt.MapFrom(src => src.valUSD))
-                .ForMember(dest => dest.PctVal, opt => opt.MapFrom(src => src.pctVal))
-                .ForMember(dest => dest.PayoffProfile, opt => opt.MapFrom(src => src.payoffProfile))
-                .ForMember(dest => dest.AssetCat, opt => opt.MapFrom(src => src.assetCat))
-                .ForMember(dest => dest.InvCountry, opt => opt.MapFrom(src => src.invCountry))
-                .ForMember(dest => dest.IsRestrictedSec, opt => opt.MapFrom(src => src.isRestrictedSec))
-                .ForMember(dest => dest.FairValLevel, opt => opt.MapFrom(src => src.fairValLevel))
-            );
-
-        var mapper = config.CreateMapper();
-        var dto = mapper.Map<Output[]>(submission.formData.invstOrSecs);
+        var dto = mapper!.Map<Output[]>(submission.formData.invstOrSecs);
 
         WriteCsv(dto, "Data/output.csv");
         logger.LogInformation("end");
